68af53b87c064ab4e390312f75a3932e49376649
WahidaFerdose <58140402+WahidaFerdose@users.noreply.github.com>
2022-10-14 02:56:41 +0600

    Added solution - LeetHub

---
+++ Pattern 7 - GFG/pattern-7.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+      
+         for(int i=1;i<=n;i++)
+        {
+            for(int k=1;k<=n-i;k++)
+            {
+                cout<<" ";
+            }
+            int j=1;
+            while(j<=2*i-1)
+            {
+                cout<<"*";
+                j++;
+
+            }
+            cout<<endl;
+
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
