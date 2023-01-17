c814570af9898342e311c1627b7c38578c62a183
WahidaFerdose <58140402+WahidaFerdose@users.noreply.github.com>
2023-01-17 12:18:06 +0600

    Add files via upload

---
+++ reversearray_recursive.cpp
@@ -0,0 +1,36 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void getans(int arr[],int s, int e)
+{
+    int sw;
+
+    if (s >= e)
+    return;
+
+    sw=arr[e];
+    arr[e]=arr[s];
+    arr[s]=sw;
+
+    getans(arr,s+1, e-1);
+
+
+
+}
+
+int main()
+{
+    int arr[]={1000, 11, 445, 1, 330, 3000};
+    int asize= sizeof(arr)/sizeof(arr[0]);
+
+
+    getans(arr,0,asize-1);
+
+    for(int i=0;i<asize;i++)
+        cout<<arr[i]<<" ";
+
+    return 0;
+}
+
