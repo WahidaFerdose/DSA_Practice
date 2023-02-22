2fe7b5df257ce0bd5405fd59c5fe6c58456bff25
WahidaFerdose <58140402+WahidaFerdose@users.noreply.github.com>
2023-02-22 10:24:46 +0600

    Added solution - LeetHub

---
+++ Doubly linked list Insertion at given position - GFG/doubly-linked-list-insertion-at-given-position.cpp
@@ -0,0 +1,133 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* a Node of the doubly linked list */
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x)
+  {
+      data = x;
+      next = prev = NULL;
+  }
+};
+
+void addNode(Node *head, int pos, int data);
+
+Node *insert(Node *head, int x)
+{
+    if (head == NULL)
+    {
+        return new Node(x);
+    }
+    Node *n = new Node(x);
+    
+    head->next = n;
+    n->prev = head;
+    head = n;
+    return head;
+}
+
+void printList(Node *head)
+{
+  // The purpose of below two loops is
+  // to test the created DLL
+  Node *temp=head;
+  if (temp != NULL) {
+ 
+  while (temp->next!=NULL)
+    temp=temp->next;
+  while (temp->prev!=NULL)
+   temp = temp->prev;
+  }
+  while (temp != NULL)
+  {
+      printf("%d ",temp->data);
+      temp=temp->next;
+  }
+  
+  cout << endl;
+}
+
+int main()
+{
+  int t;
+  scanf("%d",&t);
+  while(t--)
+  {
+  Node *head = NULL; 
+  Node *root = NULL;
+  int n;
+  scanf("%d",&n);
+  for(int i=0;i<n;i++){
+     int x;
+     scanf("%d",&x);
+     head = insert(head, x);
+     if(root==NULL) root = head;
+  }     
+  head = root;
+  int pos,data;
+  cin>>pos>>data;
+  addNode(head, pos, data);
+  printList(head);
+  }
+  return 0;
+}
+// } Driver Code Ends
+
+
+/* a Node of the doubly linked list 
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x) { data = x; next = prev = NULL; }
+}; */
+
+//Function to insert a new node at given position in doubly linked list.
+void addNode(Node *head, int pos, int data)
+{
+    Node* temp=new Node(data);
+   if(pos==-1)
+   {
+       temp->next=head;
+       head->prev=temp;
+       temp->prev=NULL;
+       head=temp;
+
+     return;
+   }
+   
+   
+   Node* t2=head;
+   
+   int c=0;
+   while(c!=pos)
+   {
+       c++;
+       t2=t2->next;
+       
+   }
+   
+  
+   if(t2->next==NULL)
+   {
+       temp->prev=t2;
+       temp->next=NULL;
+       t2->next=temp;
+       return;
+   }
+   
+    temp->prev=t2;
+   temp->next=t2->next;
+   t2->next->prev=temp;
+   t2->next=temp;
+   
+   return;
+   
+   
+}
