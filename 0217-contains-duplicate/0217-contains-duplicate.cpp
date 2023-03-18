731c77be331e168dcff93163a4add6a6759d4560
WahidaFerdose <58140402+WahidaFerdose@users.noreply.github.com>
2023-03-18 15:37:26 +0600

    Time: 179 ms (30.11%), Space: 73.5 MB (11.45%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        
+        /* Brute Force
+        bool flag=false;
+        
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    return 1;
+                }
+            }
+            
+        }
+        
+     
+        return flag;*/
+        
+        
+        //set approach
+        
+        
+        set<int> s;
+        s.insert(nums.begin(),nums.end());
+        int k=s.size();
+        
+        
+        if(n>k)
+            return 1;
+        else
+            return 0;
+        
+    }
+};
