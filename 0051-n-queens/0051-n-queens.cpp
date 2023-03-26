91a49558cab7846fbf5f075963b4aa422e872a68
WahidaFerdose <58140402+WahidaFerdose@users.noreply.github.com>
2023-03-26 08:00:33 +0600

    Time: 3 ms (94.37%), Space: 7.2 MB (66.77%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,55 +1,34 @@
 class Solution {
-    bool isSafe(vector<string> board, int row,int col,int n)
-    {
-        int drow=row;
-        int dcol=col;
-        
-        while(row>=0 && col>=0)
-        {
-            if(board[row][col]=='Q') return false;
-            row--;
-            col--;
-        }
-        row=drow;
-        col=dcol;
-        while(col>=0)
-        {
-            if(board[row][col]=='Q') return false;
-            col--;
-        }
-        
-        row=drow;
-        col=dcol;
-        while(row<n && col>=0)
-        {
-            if(board[row][col]=='Q') return false;
-            row++;
-            col--;
-        }
-        
-        return true;
-    }
-    void solve(vector<string> &board,vector<vector<string>> &ans,int col,int n)
+    void solve(vector<vector<string>> &ans, vector<string> &board, int n ,vector<int> &leftmost, vector<int> &uppD, vector<int> &lowD, int col )
     {
         if(col==n)
         {
             ans.push_back(board);
             return;
         }
+        
         for(int row=0;row<n;row++)
         {
-            if(isSafe(board,row,col,n))
+            if(leftmost[row]==0 && uppD[n-1+col-row]==0 && lowD[row+col]==0)
             {
                 board[row][col]='Q';
-                solve(board,ans,col+1,n);
-                board[row][col]='.';
+                leftmost[row]=1;
+                uppD[n-1+col-row]=1 ;
+                lowD[row+col]=1;
+                solve(ans,board,n,leftmost,uppD,lowD,col+1);
+                 board[row][col]='.';
+                leftmost[row]=0;
+                uppD[n-1+col-row]=0;
+                lowD[row+col]=0;
+                   
+                
             }
         }
-        
     }
+
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
+       vector<vector<string>> ans;
         vector<string> board(n);
         string s(n,'.');
         for(int i=0;i<n;i++)
@@ -57,7 +36,9 @@ public:
             board[i]=s;
         }
         
-        solve(board,ans,0,n);
+        vector<int> leftmost(n,0), uppD(2*n-1,0),lowD(2*n-1);
+        
+        solve(ans,board,n,leftmost,uppD,lowD,0);
         return ans;
         
     }
